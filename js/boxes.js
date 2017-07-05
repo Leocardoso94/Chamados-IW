a0035d640ff31ee7187903a0295883312d078e1e
Leonardo Cardoso <leocardosoti@gmail.com>
2017-07-05 20:40:42 -0300

    first commit

---
+++ js/boxes.js
@@ -0,0 +1,148 @@
+var boxes = [
+{
+	title: 'Inclusão/Acesso Report Server V1',
+	link: 'https://iw.claro.com.br/v1/mod/index.php?MID=fsv&GID=usrgrp&ACT=',
+	modulo: 'Dashboard',
+	textsAndImgs: [
+	{
+		text:'Localizar o usuário pelo nome ou pela matrícula e clicar em alterar',
+		img:'1.JPG'
+	},
+	{
+		text: 'Após isso selecionar o relatório desejado e clicar em atribuir',
+		img:'1.1.JPG'
+	}
+	]
+},
+{
+	title: 'Inclusão/Acesso Report Server V2',
+	link: 'https://iw.claro.com.br/v2/sys/gerenciamento/extracao_sql',
+	modulo: 'Administração',
+	textsAndImgs: [
+	{
+		text:'Localizar o relatório pelo número e clicar em alterar',
+		img:'2.JPG'
+	},
+	{
+		text: 'Após isso clicar em novo, inserir o login e clicar em salvar e voltar',
+		img:'2.1.JPG'
+	}
+	]
+
+},
+{
+	title: 'Inclusão na Hierarquia',
+	link: 'https://igarapava.claro.com.br/v1/mod/index.php?MID=adm&GID=dadosrh&ACT=',
+	modulo: 'Gestão de Demandas',
+	textsAndImgs: [
+	{
+		text:'Localizar o usuário pelo login e clicar em alterar',
+		img:'3.JPG'
+	},
+	{
+		text: 'Após isso clicar no ícone, localizar o gestor pelo login e/ou nome, clicar no check ✓. Finalizar clicando no ok',
+		img:'3.1.JPG'
+	}
+	]
+
+},
+{
+	title: 'Representação no IW GD',
+	link: '',
+	modulo: '',
+	textsAndImgs: [
+	{
+		text:'',
+		img:''
+	},
+	{
+		text: '',
+		img:''
+	}
+	]
+
+},
+{
+	title: 'Administração IW GD',
+	link: '',
+	modulo: 'Gestão de Demandas',
+	textsAndImgs: [
+	{
+		text:'Busque pelo id demanda',
+		img:''
+	},
+	{
+		text: 'Aqui existem várias possibilidades busque no menu ao lado a funcionalidade que se encaixa com a solicitação do usuário',
+		img:''
+	}
+	]
+
+},
+{
+	title: 'Alteração de Grupos Aprovadores de Atividades BSR',
+	link: '',
+	modulo: 'Gestão de Demandas',
+	textsAndImgs: [
+	{
+		text:'',
+		img:''
+	},
+	{
+		text: '',
+		img:''
+	}
+	]
+
+},
+{
+	title: 'Voltar Status da O.S',
+	link: '',
+	modulo: 'Gestão de Demandas',
+	textsAndImgs: [
+	{
+		text:'Para voltar o status da OS é necessário acessar o menu de Administração de Demandas',
+		img:''
+	},
+	{
+		text: 'Selecione o status desejado e clique em alterar',
+		img:''
+	}
+	]
+
+},
+{
+	title: 'Cadastro de Business Running',
+	link: '',
+	modulo: 'Gestão de Demandas',
+	textsAndImgs: [
+	{
+		text:'',
+		img:''
+	},
+	{
+		text: '',
+		img:''
+	}
+	]
+
+}
+];
+
+/*
+{
+	title: '',
+	link: '',
+	modulo: '',
+	textsAndImgs: [
+	{
+		text:'',
+		img:''
+	},
+	{
+		text: '',
+		img:''
+	}
+	]
+
+}
+*/
